dbe70e3dd2931d32c5472f132312e035e6a272c6
bellots <andreabellotto88@gmail.com>
2024-12-08 02:10:48 +0100

    done funny thing

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -11,27 +11,37 @@ import SwiftUIThemeFlow
 struct ContentView: View {
     var body: some View {
         ThemeFlow(themes: [MyCustomTheme.primitiveTitle], content: {
-            Text("Hello, world!")
-                .themeable(MyCustomTheme.primitiveBoldTitle)
+            ForEach(MyCustomTheme.allCases.indices, id: \.self) { index in
+                Text(MyCustomTheme.allCases[index].rawValue)
+                    .themeable(MyCustomTheme.allCases[index])
+            }
         })
     }
 }
 
-enum MyCustomTheme: Theme {
+enum MyCustomTheme: String, Theme, CaseIterable {
     case primitiveTitle
     case primitiveBoldTitle
+    case primitiveBoldTitleRounded
     
-
-    func apply<V: View>(to view: V) -> AnyView {
+    private func customStyle<V: View>(to view: V) -> any View {
         switch self {
         case .primitiveTitle:
-            return AnyView(view
-                .foregroundColor(.red)
-                    )
+            return view.foregroundColor(.red)
         case .primitiveBoldTitle:
-            return AnyView(MyCustomTheme.primitiveTitle.apply(to: view)
+            return MyCustomTheme.primitiveTitle
+                .apply(to: view)
                 .font(.largeTitle)
-                           )
+                .padding(5)
+                .background(Color.green)
+        case .primitiveBoldTitleRounded:
+            return MyCustomTheme.primitiveBoldTitle
+                .apply(to: view)
+                .cornerRadius(10)
         }
+    }
+
+    func apply<V: View>(to view: V) -> AnyView {
+        return AnyView(customStyle(to: view))
     }
 }
